867a0034e43f2802f44544847252150c8964368a
Michael Cui <mlcui1@student.monash.edu>
2017-01-12 20:55:03 +1100

    Point: Add method to reflect across a line

--- structs/point.h
+++ structs/point.h
@@ -57,6 +57,27 @@ struct Point
 	{
 		return std::sqrt(this->distance_sq(other));
 	}
+
+	// Reflects the point across the line lr.
+	Point reflect(const Point& l, const Point& r) const
+	{
+		const double D = r.distance_sq(l);
+
+		// I have no idea how the below works.
+		// Let X be the array [ly, rx, py, lx, ry, px].
+		// magic is equal to the the alternating sum of (product of pair)
+		// for all adjacent pairs (wrapping around) of X.
+		#define p this
+		const double magic = (p->x * l.y) - (l.y * r.x) + (r.x * p->y) 
+							- (p->y * l.x) + (l.x * r.y) - (r.y * p->x);
+		#undef p
+
+		// The vector r - l rotated 90 degrees counterclockwise.
+		// Can imagine "multiplying" the vector by the imaginary constant.
+		const Point delta_rotated = {l.y - r.y, r.x - l.x};
+
+		return *this - (2.0 * magic / D) * delta_rotated;
+	}
 };
 
 }
