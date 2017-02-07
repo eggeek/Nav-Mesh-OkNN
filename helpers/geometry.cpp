ac1b729144c69890b0786f0210949009e4af4908
Michael Cui <mlcui1@student.monash.edu>
2017-02-07 19:22:52 +1100

    Inline geometry helper functions

--- helpers/geometry.cpp
+++ helpers/geometry.cpp
@@ -39,56 +39,6 @@ void line_intersect_time(const Point& a, const Point& b,
     }
 }
 
-// Returns where num / denom is in the range [0, 1].
-ZeroOnePos line_intersect_bound_check(const double num, const double denom)
-{
-    // Check num / denom == 0.
-    if (std::abs(num) < EPSILON)
-    {
-        return ZeroOnePos::EQ_ZERO;
-    }
-    // Check num / denom == 1.
-    // Note: Checking whether it is accurately near 1 requires us to check
-    // |num - denom| < EPSILON^2 * denom
-    // which requires a multiplication. Instead, we can assume that denom
-    // is less than 1/EPSILON and check
-    // |num - denom| < EPSILON
-    // instead. This is less accurate but faster.
-    if (std::abs(num - denom) < EPSILON)
-    {
-        return ZeroOnePos::EQ_ONE;
-    }
-
-    // Now we finally check whether it's greater than 1 or less than 0.
-    if (denom > 0)
-    {
-        if (num < 0)
-        {
-            // strictly less than 0
-            return ZeroOnePos::LT_ZERO;
-        }
-        if (num > denom)
-        {
-            // strictly greater than 1
-            return ZeroOnePos::GT_ONE;
-        }
-    }
-    else
-    {
-        if (num > 0)
-        {
-            // strictly less than 0
-            return ZeroOnePos::LT_ZERO;
-        }
-        if (num < denom)
-        {
-            // strictly greater than 1
-            return ZeroOnePos::GT_ONE;
-        }
-    }
-    return ZeroOnePos::IN_RANGE;
-}
-
 // Reflects the point across the line lr.
 Point reflect_point(const Point& p, const Point& l, const Point& r)
 {
@@ -114,22 +64,6 @@ Point reflect_point(const Point& p, const Point& l, const Point& r)
     #endif
 
     return p + (2.0 * numer / denom) * delta_rotated;
-}
-
-Orientation get_orientation(const Point& a, const Point& b, const Point& c)
-{
-    const double cross = (b - a) * (c - b);
-    if (std::abs(cross) < EPSILON)
-    {
-        return Orientation::COLLINEAR;
-    } else if (cross > 0)
-    {
-        return Orientation::CCW;
-    }
-    else
-    {
-        return Orientation::CW;
-    }
 }
 
 }
--- helpers/geometry.h
+++ helpers/geometry.h
@@ -27,7 +27,58 @@ enum struct ZeroOnePos
     GT_ONE,   // n > 1
 };
 
-ZeroOnePos line_intersect_bound_check(const double num, const double denom);
+// Returns where num / denom is in the range [0, 1].
+inline ZeroOnePos line_intersect_bound_check(
+    const double num, const double denom
+)
+{
+    // Check num / denom == 0.
+    if (std::abs(num) < EPSILON)
+    {
+        return ZeroOnePos::EQ_ZERO;
+    }
+    // Check num / denom == 1.
+    // Note: Checking whether it is accurately near 1 requires us to check
+    // |num - denom| < EPSILON^2 * denom
+    // which requires a multiplication. Instead, we can assume that denom
+    // is less than 1/EPSILON and check
+    // |num - denom| < EPSILON
+    // instead. This is less accurate but faster.
+    if (std::abs(num - denom) < EPSILON)
+    {
+        return ZeroOnePos::EQ_ONE;
+    }
+
+    // Now we finally check whether it's greater than 1 or less than 0.
+    if (denom > 0)
+    {
+        if (num < 0)
+        {
+            // strictly less than 0
+            return ZeroOnePos::LT_ZERO;
+        }
+        if (num > denom)
+        {
+            // strictly greater than 1
+            return ZeroOnePos::GT_ONE;
+        }
+    }
+    else
+    {
+        if (num > 0)
+        {
+            // strictly less than 0
+            return ZeroOnePos::LT_ZERO;
+        }
+        if (num < denom)
+        {
+            // strictly greater than 1
+            return ZeroOnePos::GT_ONE;
+        }
+    }
+    return ZeroOnePos::IN_RANGE;
+}
+
 // Given two points a, b and a number t, compute the point
 //  a + (b-a) * t
 inline Point get_point_on_line(const Point& a, const Point& b, const double t)
@@ -43,7 +94,24 @@ enum struct Orientation
 	CW,       // clockwise
 };
 
-Orientation get_orientation(const Point& a, const Point& b, const Point& c);
+inline Orientation get_orientation(
+    const Point& a, const Point& b, const Point& c
+)
+{
+    const double cross = (b - a) * (c - b);
+    if (std::abs(cross) < EPSILON)
+    {
+        return Orientation::COLLINEAR;
+    } else if (cross > 0)
+    {
+        return Orientation::CCW;
+    }
+    else
+    {
+        return Orientation::CW;
+    }
+}
+
 inline bool is_collinear(const Point& a, const Point& b, const Point& c)
 {
     return std::abs((b - a) * (c - b)) < EPSILON;
