bcf30a52e301aad0902384184dce808a03409c39
eggeek <eggeek.me@gmail.com>
2018-01-18 20:43:54 +1100

    generate random pts in tight border

--- gen.cpp
+++ gen.cpp
@@ -26,6 +26,7 @@ void gen_poly(string parkpath, double size, int num) {
 
   //vector<vector<pl::Point>> normalized;
   generator::normalize_polys(polys, size);
+  polys = generator::remove_bad_polys(polys);
 
   vector<vector<pl::Point>> simplified;
   generator::simplify_polys(polys, simplified);
@@ -37,15 +38,27 @@ void gen_entities_points(string polypath, string meshpath, int num) {
   ifstream meshfile(meshpath);
   polyanya::Mesh* mp = new polyanya::Mesh(meshfile);
   EDBT::ObstacleMap* oMap = new EDBT::ObstacleMap(polyfile, mp);
+
+  // from *.poly2 -> *.poly
+  polypath.pop_back();
+  ifstream polysfile(polypath);
+  vector<vector<pl::Point>> polys = generator::read_polys(polysfile);
   long long min_x, max_x, min_y, max_y;
-  min_x = max_x = oMap->vs[0].x;
-  min_y = max_y = oMap->vs[0].y;
-  for (int i=0; i<(int)oMap->vs.size(); i++) {
-    min_x = min(min_x, oMap->vs[i].x);
-    max_x = max(max_x, oMap->vs[i].x);
-    min_y = min(min_y, oMap->vs[i].y);
-    max_y = max(max_y, oMap->vs[i].y);
+  // ignore border
+  min_x = max_x = polys[1][0].x;
+  min_y = max_y = polys[1][0].y;
+  for (size_t i=1; i<polys.size(); i++) {
+    for (const auto& p: polys[i]) {
+      min_x = min(min_x, (long long)p.x);
+      max_x = max(max_x, (long long)p.x);
+      min_y = min(min_y, (long long)p.y);
+      max_y = max(max_y, (long long)p.y);
+    }
   }
+  cerr << "border: " << polys[0][0] << " " << polys[0][1] << endl;
+  cerr << "x: " << min_x << " " << max_x << endl;
+  cerr << "y: " << min_y << " " << max_y << endl;
+
   random_device rd;
   mt19937 eng(rd());
   uniform_int_distribution<long long> distx(min_x, max_x);
