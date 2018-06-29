8124cb8e9d3eab49b964087a7cf82f519e0d4007
eggeek <eggeek.me@gmail.com>
2018-06-29 15:34:05 +1000

    add test cases for brute-force polyanya

--- test.cpp
+++ test.cpp
@@ -15,7 +15,6 @@ using namespace polyanya;
 
 MeshPtr mp;
 Mesh m;
-Point tp;
 EDBT::ObstacleMap* oMap;
 EDBT::EDBTkNN* edbt;
 SearchInstance* si;
@@ -27,7 +26,6 @@ KnnMeshEdgeFence * meshFence;
 vector<Scenario> scenarios;
 vector<Point> pts;
 vector<vector<Point>> polys;
-Point start;
 
 void load_points(istream& infile ) {
   int N;
@@ -52,7 +50,7 @@ void load_data() {
   load_points(ptsfile);
   mp = new Mesh(meshfile);
   m = *mp;
-  //oMap = new EDBT::ObstacleMap(obsfile, &m);
+  oMap = new EDBT::ObstacleMap(obsfile, &m);
   meshfile.close();
   si = new SearchInstance(mp);
   ki = new OkNNIntervalHeuristic(mp);
@@ -65,47 +63,76 @@ void load_data() {
 }
 
 TEST_CASE("Test polyanya zero heuristic") {
-	ki->set_K(pts.size());
-	ki->set_start_goal(start, pts);
+  int N = 10;
+  vector<Point> starts;
+  generator::gen_points_in_traversable(oMap, polys, N, starts);
 
-	ki0->set_K(pts.size());
-	ki0->set_start_goal(start, pts);
+  for (Point& start: starts) {
+    ki->set_K(pts.size());
+    ki->set_start_goal(start, pts);
 
-	int res0 = ki0->search();
-	int res = ki->search();
-	REQUIRE(res0 == res);
-	for (int i=0; i<res0; i++) {
-		double d0, d;
-		d0 = ki0->get_cost(i);
-		d = ki->get_cost(i);
-		REQUIRE(fabs(d0 - d) < EPSILON);
-	}
+    ki0->set_K(pts.size());
+    ki0->set_start_goal(start, pts);
+
+    int res0 = ki0->search();
+    int res = ki->search();
+    REQUIRE(res0 == res);
+    for (int i=0; i<res0; i++) {
+      double d0, d;
+      d0 = ki0->get_cost(i);
+      d = ki->get_cost(i);
+      REQUIRE(fabs(d0 - d) < EPSILON);
+    }
+  }
 }
 
 TEST_CASE("Test target heuristic") {
-	hi->set_K(pts.size());
-	hi->set_start(start);
-	hi->set_goals(pts);
+  int N = 10;
+  vector<Point> starts;
+  generator::gen_points_in_traversable(oMap, polys, N, starts);
+  for (Point& start: starts) {
+    hi->set_K(pts.size());
+    hi->set_start(start);
+    hi->set_goals(pts);
 
-	ki->set_K(pts.size());
-	ki->set_start_goal(start, pts);
+    ki->set_K(pts.size());
+    ki->set_start_goal(start, pts);
 
-	int reshi = hi->search();
-	int reski = ki->search();
-	REQUIRE(reski == reshi);
-	for (int i=0; i<reshi; i++) {
-		double dhi, dki;
-		dhi = hi->get_cost(i);
-		dki = ki->get_cost(i);
-		REQUIRE(fabs(dhi - dki) < EPSILON);
-	}
+    int reshi = hi->search();
+    int reski = ki->search();
+    REQUIRE(reski == reshi);
+    for (int i=0; i<reshi; i++) {
+      double dhi, dki;
+      dhi = hi->get_cost(i);
+      dki = ki->get_cost(i);
+      REQUIRE(fabs(dhi - dki) < EPSILON);
+    }
+  }
+}
+
+TEST_CASE("Test brute force polyanya") {
+  int N = 10;
+  vector<Point> starts;
+  generator::gen_points_in_traversable(oMap, polys, N, starts);
+  for (Point& start: starts) {
+    int k = min(5, (int)pts.size());
+    double cost_poly = 0, gen_poly = 0;
+    vector<double> dists = si->brute_force(start, pts, k, cost_poly, gen_poly);
+
+    ki->set_K(k);
+    ki->set_start_goal(start, pts);
+    int reski = ki->search();
+    REQUIRE(reski == (int)dists.size());
+    for (int i=0; i<reski; i++) {
+      double dki = ki->get_cost(i);
+      REQUIRE(fabs(dki - dists[i]) < EPSILON);
+    }
+  }
 }
 
 int main(int argv, char* args[]) {
 	cout << "Loading data..." << endl;
   load_data();
-	start = pts.back(); pts.pop_back();
-
 	cout << "Running test cases..." << endl;
 	Catch::Session session;
 	int res = session.run(argv, args);
