605f08a443b868a454a81523dfb94ff04b85de93
eggeek <eggeek.me@gmail.com>
2018-07-13 18:26:59 +1000

    clean analysis

---
+++ analysis/SoCS18.py
@@ -0,0 +1,305 @@
+import utilities as ut
+from utilities import alias
+from utilities import plot_graph
+from utilities import gen_xy
+
+def experiment1_dense_time(DF, time_ylim=None):
+  saveto = './figs/e1_dense_time.png'
+  size = 50
+  blimit = 10
+  df = DF.copy()
+  df = df[df.k == 1]
+  df.dist = (df.dist / size).astype(int)
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'dist', 'cost_ki0', limit=blimit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'dist', 'cost_ki', limit=blimit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'dist', 'cost_hi', limit=blimit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'dist', 'cost_edbt', limit=blimit)
+  algos.append('LVG')
+
+  for k in xs.keys():
+      xs[k] = [v * size for v in xs[k]]
+      ys[k] = [v / 1000 for v in ys[k]]
+#     ys[k] = np.log10(ys[k])
+      
+  ut.plot_graph('dist', 'time(ms) (log10)', list(xs.values()), list(ys.values()), algos, 
+             ylim=time_ylim, saveto=saveto)
+
+
+def experiment1_dense_gen(DF, gen_ylim=None):
+  # dense: gen
+  saveto = './figs/e1_dense_gen.png'
+  size = 50
+  limit = 10
+  df = DF.copy()
+  df = df[df.k == 1]
+  df.dist = (df.dist / size).astype(int)
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'dist', 'gen_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'dist', 'gen_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'dist', 'gen_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'dist', 'gen_edbt', limit=limit)
+  algos.append('LVG')
+
+  for k in xs.keys():
+      xs[k] = [v * size for v in xs[k]]
+#     ys[k] = np.log10(ys[k])
+      
+  ut.plot_graph('dist', 'generated (log10)', list(xs.values()), list(ys.values()), algos, 
+             ylim=gen_ylim, saveto=saveto)
+
+
+def experiment1_sparse_time(DF, time_ylim):
+  # sparse: time
+  saveto = './figs/e1_sparse_time.png'
+  limit = 10
+  size = 500
+  df = DF.copy()
+  df = df[(df.k == 1) & (df.pts == 10)]
+  df.dist = (df.dist / size).astype(int)
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'dist', 'cost_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'dist', 'cost_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'dist', 'cost_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'dist', 'cost_poly', limit=limit)
+  algos.append(alias['polyanya'])
+
+  for k in xs.keys():
+      xs[k] = [v * size for v in xs[k]]
+      ys[k] = [v / 1000 for v in ys[k]]
+      
+  ut.plot_graph('dist', 'time(ms) (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=time_ylim, saveto=saveto)
+
+def experiment1_sparse_gen(DF, gen_ylim):
+  # sparse: gen
+  saveto = './figs/e1_sparse_gen.png'
+  limit = 10
+  size = 500
+  df = DF.copy()
+  df = df[(df.k == 1) & (df.pts == 10)]
+  df.dist = (df.dist / size).astype(int)
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'dist', 'cost_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'dist', 'gen_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'dist', 'gen_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'dist', 'gen_poly', limit=limit)
+  algos.append(alias['polyanya'])
+
+  for k in xs.keys():
+      xs[k] = [v * size for v in xs[k]]
+      
+  ut.plot_graph('dist', 'generated (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=gen_ylim, saveto=saveto)
+
+def experiment2_dense_time(DF, time_ylim):
+  # dense: time
+  saveto = './figs/e2_dense_time.png'
+  limit = 10
+  df = DF.copy()
+  df = df[(df.polys >= 8000) & (df.k <= 10)]
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'k', 'cost_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'k', 'cost_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'k', 'cost_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'k', 'cost_edbt', limit=limit)
+  algos.append('LVG')
+
+  for k in xs.keys():
+      xs[k] = [v for v in xs[k]]
+      ys[k] = [v / 1000 for v in ys[k]]
+      
+  ut.plot_graph('k', 'time(ms) (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=time_ylim, saveto=saveto)
+
+def experiment2_dense_gen(DF, gen_ylim):
+  # dense: gen
+  saveto = './figs/e2_dense_gen.png'
+
+  limit = 10
+  df = DF.copy()
+  df = df[(df.polys >= 8000) & (df.k <= 10)]
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'k', 'cost_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'k', 'gen_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'k', 'gen_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'k', 'gen_edbt', limit=limit)
+  algos.append('LVG')
+
+  for k in xs.keys():
+      xs[k] = [v for v in xs[k]]
+#     ys[k] = np.log10(ys[k])
+      
+  ut.plot_graph('k', 'generated (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=gen_ylim, saveto=saveto)
+
+
+def experiment2_sparse_time(DF, time_ylim):
+  # sparse: time
+  saveto = './figs/e2_sparse_time.png'
+  limit = 10
+  df = DF.copy()
+  df = df[(df.polys >= 8000) & (df.pts == 10)]
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'k', 'cost_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = ut.gen_xy(df, 'k', 'cost_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = ut.gen_xy(df, 'k', 'cost_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = ut.gen_xy(df, 'k', 'cost_poly', limit=limit)
+  algos.append(alias['polyanya'])
+
+  for k in xs.keys():
+      xs[k] = [v for v in xs[k]]
+      ys[k] = [v / 1000 for v in ys[k]]
+#     ys[k] = np.log10(ys[k])
+  ut.plot_graph('k', 'time(ms) (log10)', list(xs.values()), list(ys.values()), algos, yscale='log',
+             ylim=time_ylim, saveto=saveto)
+
+
+def experiment2_sparse_gen(DF, gen_ylim):
+  # sparse: expanded
+  saveto = './figs/e2_sparse_gen.png'
+  limit = 10
+  df = DF.copy()
+  df = df[(df.polys >= 8000) & (df.pts == 10)]
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'k', 'gen_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = gen_xy(df, 'k', 'gen_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = gen_xy(df, 'k', 'gen_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = gen_xy(df, 'k', 'cost_poly', limit=limit)
+  algos.append(alias['polyanya'])
+
+  for k in xs.keys():
+      xs[k] = [v for v in xs[k]]
+#     ys[k] = np.log10(ys[k])
+  plot_graph('k', 'generated (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=gen_ylim, saveto=saveto)
+
+
+def experiment3_time(DF, time_ylim):
+  # sparse: time
+  saveto = './figs/e3_time.png'
+  limit = 10
+  df = DF.copy()
+  df = df[(df.polys >= 8000) & (df.k == 1) & (df.pts <= 10)]
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'pts', 'cost_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = gen_xy(df, 'pts', 'cost_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = gen_xy(df, 'pts', 'cost_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = gen_xy(df, 'pts', 'cost_poly', limit=limit)
+  algos.append(alias['polyanya'])
+
+  for i in xs.keys():
+      xs[i] = [v for v in xs[i]]
+      ys[i] = [v / 1000 for v in ys[i]]
+      
+  plot_graph('target', 'time(ms) (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=time_ylim, saveto=saveto)
+
+def experiment3_gen(DF, gen_ylim):
+  # sparse: expaned
+  saveto = './figs/e3_gen.png'
+  limit = 10
+  df = DF.copy()
+  df = df[(df.polys >= 8000) & (df.k == 1) & (df.pts <= 10)]
+  xs = {}
+  ys = {}
+  algos = []
+
+# xs[0], ys[0] = gen_xy(df, 'pts', 'gen_ki0', limit=limit)
+# algos.append(alias['poly-zero'])
+
+  xs[1], ys[1] = gen_xy(df, 'pts', 'gen_ki', limit=limit)
+  algos.append(alias['interval heuristic'])
+
+  xs[2], ys[2] = gen_xy(df, 'pts', 'gen_hi', limit=limit)
+  algos.append(alias['target heuristic'])
+
+  xs[3], ys[3] = gen_xy(df, 'pts', 'gen_poly', limit=limit)
+  algos.append(alias['polyanya'])
+
+  for i in xs.keys():
+      xs[i] = [v for v in xs[i]]
+#     ys[i] = np.log10(ys[i])
+  plot_graph('target', 'generated (log10)', list(xs.values()), list(ys.values()), algos,
+             ylim=gen_ylim, saveto=saveto)
---
+++ analysis/utilities.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+alias = {}
+styles = {}
+
+def load_data(fname):
+    with open(fname, "r") as f:
+        # ignore `vertices: ...` at head and `Total: ..` at tail
+        raws = f.readlines()
+    header = [i.strip() for i in raws[0].split(',')]
+    lines = [[j.strip() for j in i.split(',')] for i in raws[1:]]
+    t = pd.DataFrame.from_records(lines, columns=header)
+    res = t[header].apply(pd.to_numeric, errors='ignore')
+    return res
+
+def load_files(paths):
+    frames = []
+    for i in paths:
+        print(i)
+        frames.append(load_data(i))
+    res = pd.concat(frames)
+    return res
+
+def gen_xy(df=None, colx='', coly='', ignore=True, limit=20):
+    tg = df.groupby(colx)
+    x = []
+    y = []
+    for k, v in tg[coly].apply(lambda _: np.average(_)).items():
+        if ignore and tg.size()[int(k)] < limit:
+            continue
+        x.append(int(k))
+        y.append(v)
+    return x, y
+
+def plot_graph(xlabel='', ylabel='', xs=[[]], ys=[[]], labels=[], color=None, 
+               yscale='log', ylim=None, saveto=None):
+    
+    fig, ax = plt.subplots()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_yscale(yscale)
+    if ylim is not None:
+        ax.set_ylim(ylim)
+    n = len(xs)
+    for i in range(n):
+        x = xs[i]
+        y = ys[i]
+        if styles.get(labels[i]) is not None:
+            plt.plot(x, y, styles[labels[i]], label=labels[i])
+        else:
+            ax.scatter(x, y)
+            ax.plot(x, y, label=labels[i])
+    ax.legend(labels)
+    ax.legend(loc='best', fancybox=True, framealpha=0, prop={'size': 14})
+    plt.grid(True)
+    if saveto is not None:
+        fig.savefig(saveto, bbox_inches='tight')
