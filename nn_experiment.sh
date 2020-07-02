5d4d600ad13a8b2e48c256c5f39468fba9c85230
eggeek <eggeek.me@gmail.com>
2020-07-02 16:08:57 +1000

    ignore polygons & meshes data

--- nn_experiment.sh
+++ nn_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 map="9000"
 for ((i=1; i<=1000; i*=10)) {
-  r=`python -c "print $i/10000.0"`
+  r=`python -c "print ($i/10000.0)"`
   cmd="./bin/experiment nn $r < ./inputs/${map}.in > ./outputs/nn/${map}-$i.log"
   echo $cmd
   eval $cmd
